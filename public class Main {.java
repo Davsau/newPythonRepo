e57d1c710d73740f58a69a84959d7ca313eb3646
David Lopez <davidlopezoreos02@gmail.com>
2023-01-29 02:20:00 -0800

    this is a message

---
+++ public class Main {.java
@@ -0,0 +1,28 @@
+public class Main {
+    
+
+    public static void main(String[] args){
+    
+        Cat.meow();
+
+    Cat myCat = new Cat;
+    myCat.name() = "Fred";
+    myCat.age = 6;
+    myCat.meow
+
+    Cat anotherCat = new Cat();
+    anotherCat.name = "Stella";
+    anotherCat.age = 5;
+
+    System.out.println(myCat.age);
+}
+
+private static String printName(String name, int number)
+ 
+  if (number ! == 5) {
+    System.out.println("This guy is awesome");
+  }
+   else if (name.equals("Larry")){
+    System.out.println("This guy is OK I guess");
+   }
+}
